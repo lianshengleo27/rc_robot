ccda211379d5e3a6ae91a2575b7248eb4bd9278a
lianshengleo27 <lianshengleo@gmail.com>
2024-07-30 00:41:35 +0900

    change robot name

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('rc_dump'))
+    pkg_path = os.path.join(get_package_share_directory('rc_robot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
 
